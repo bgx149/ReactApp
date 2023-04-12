f6b972a0d338eb7c4388a7a08b8dea5403940979
Alex Kiippa <alex.kiippa@myy.haaga-helia.fi>
2023-04-12 10:16:11 +0300

    carshop

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import Carlist from './components/Carlist'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+
+
+function App() {
+
+
+
+
+
+
+
+
+
+  return (
+    <div className="App">
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+           
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          My Carshop
+          </Typography>
+         
+        </Toolbar>
+      </AppBar>
+    </Box>
+
+
+      <Carlist/>
+    </div>
+  );
+}
+
+export default App;
